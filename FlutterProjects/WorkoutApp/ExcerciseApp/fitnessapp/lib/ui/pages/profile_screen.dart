634f607801441965c6cc07ce8ea62b4c8114c5e0
Joeylamanna <josephlamanna03@gmail.com>
2022-12-02 18:45:46 -0500

    updated profile

--- FlutterProjects/WorkoutApp/ExcerciseApp/fitnessapp/lib/ui/pages/profile_screen.dart
+++ FlutterProjects/WorkoutApp/ExcerciseApp/fitnessapp/lib/ui/pages/profile_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -7,6 +9,22 @@ class ProfileScreen extends StatelessWidget {
 
     return Scaffold(
         backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        
+        bottomNavigationBar: BottomNavigationBar(
+                  backgroundColor: Color.fromARGB(255, 0, 0, 0),
+          items: [
+          BottomNavigationBarItem( 
+           icon: Icon(Icons.home, color:Color.fromARGB(255, 1, 217, 255)),
+          label:  "Home", ),
+           BottomNavigationBarItem( 
+           icon: Icon(Icons.timer,color:Color.fromARGB(255, 1, 217, 255)),
+          label:  "Timer", ),
+          BottomNavigationBarItem( 
+           icon: Icon(Icons.grade_outlined, color:Color.fromARGB(255, 1, 217, 255)),
+          label:  "Personal Records ", ),
+        ],
+      ),
+        
         body: Stack(
           children: <Widget>[
             Positioned(
@@ -16,6 +34,7 @@ class ProfileScreen extends StatelessWidget {
               right: 0,
               child: ClipRRect(
                 borderRadius: const BorderRadius.vertical(
+                  //top: const Radius.circular(40),
                   bottom: const Radius.circular(40),
                 ),
                 child: Container(
